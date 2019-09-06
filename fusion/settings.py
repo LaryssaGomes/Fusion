ce5144371c828e643d393264b9a22b3791882d80
Geek University <contato@geekuniversity.com.br>
2019-09-06 15:51:04 -0300

    fiz

--- fusion/settings.py
+++ fusion/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+# https://docs.dddsdjangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
